bc90cb2ff4e8e141014f14a355ac3f29f5786194
coffeeandpouch <sameinsidler@me.com>
2021-04-28 09:57:14 -0500

    commits changes to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "../src/Header/Header";
+import SearchForm from "../src/SearchForm/SearchForm";
 
 function App() {
-  return <main className="App">{<Header />}</main>;
+  return (
+    <main className="App">
+      {<Header />}
+      {<SearchForm />}
+    </main>
+  );
 }
 
 export default App;
