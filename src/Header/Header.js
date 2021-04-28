cd86a0c6d53d26653214a294c787d5b71875f2d1
coffeeandpouch <sameinsidler@me.com>
2021-04-28 09:48:36 -0500

    changes div tag to frag in Header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,10 +3,10 @@ import "./Header.css";
 
 function Header() {
   return (
-    <div>
+    <>
       <h1>NextUpSports</h1>
       <h2>The one stop shop to check out who your team is playing next</h2>
-    </div>
+    </>
   );
 }
 
