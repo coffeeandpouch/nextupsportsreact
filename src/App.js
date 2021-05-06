09661b3ab65e8069c28fdd7c7807e3c19e5bdf21
coffeeandpouch <sameinsidler@me.com>
2021-05-06 13:51:48 -0500

    refresh src folder

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import React from "react";
 import Header from "../src/Header/Header";
-import GetLeagues from "../src/GetLeagues/GetLeagues";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      League: "Coconut",
-      Team: [],
-    };
-  }
-  handleChange = (event) => {
-    this.setState({ value: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    alert(" " + this.state.value);
-    event.preventDefault();
-  };
-  render() {
-    return (
-      <div>
-        <main className="App">
-          {<Header />}
-
-          <br />
-          <GetLeagues />
-        </main>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
