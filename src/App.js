90982153724de523cc1fcdee9434c9dc2e3d00fe
coffeeandpouch <sameinsidler@me.com>
2021-05-07 09:49:23 -0500

    commits  changes made to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
 import Header from "../src/Header/Header";
+import GetLeagues from "../src/GetLeagues/GetLeagues";
 
 export default function App() {
   return (
-    <div className="App">
+    <div name="App">
       <Header />
+      <div className="leagues" />
+      <GetLeagues />
     </div>
   );
 }
