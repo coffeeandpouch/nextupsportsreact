4ae504cde9116e065dd3fba39411ae7be2826761
coffeeandpouch <sameinsidler@me.com>
2021-05-07 09:47:49 -0500

    commits changes made to Header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,9 +3,11 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <>
-      <h1>NextUpSports</h1>
-      <h2>The one stop shop to check out who your team is playing next</h2>
-    </>
+    <div className="titles">
+      <h1 id="title">NextUpSports</h1>
+      <h2 id="subtitle">
+        The one stop shop to check out who your team is playing next
+      </h2>
+    </div>
   );
 }
